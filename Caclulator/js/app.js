bfc30c60982b6672540afc73f9e811db196a301c
DXCannon <prestondcannon@gmail.com>
2022-12-05 12:42:43 -0800

    Calculator

---
+++ Caclulator/js/app.js
@@ -0,0 +1,31 @@
+// Store results in global variable
+const result = document.getElementById("answer");
+
+
+
+// Declare function
+function resultAssign(){
+    num = 3 + 5 + 500 + 87 + 2;
+    result.innerHTML = num;
+    console.log("Variable num represents the number: " + num);
+
+}
+// Call the function
+// resultAssign()
+
+// Function to add two and two
+// Declare functiom
+function addTwoPlusTwo() {
+    result.innerHTML = 2 + 2;
+    
+}
+function dividOneHundredFortyFourbyTwo(){
+    result.innerHTML = 144 / 2;
+}
+
+function multiplyThreeTimesFour(){
+    result.innerHTML = 3 * 4;
+}
+function subtractFifteenfromThirtyFive(){
+    result.innerHTML = 35 - 15;
+}
